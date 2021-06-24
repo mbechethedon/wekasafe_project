c4e06e051273ba072cc64f8c771dc1ce733c9d51
Mbeche <71378504+Mbeche@users.noreply.github.com>
2021-06-24 14:46:52 +0300

    added dashboard page

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -6,7 +6,27 @@ class AuthController {
 
      async postLogin({ request, auth, response}) {
         await auth.attempt(request.input('email'), request.input('password'))
-        return response.route('index')
+        return response.route('dashboard')
+    }
+      registrationView({ view }) {
+        return view.render('register')
+    }
+    async postRegister({ request, session, response }) {
+        const user = await User.create({
+            username: request.input('name'),
+            email: request.input('email'),
+            password: request.input('password')
+        })
+        session.flash({ successmessage: 'User have been created successfully'})
+        return response.route('/');
+    }
+    renderDashboard({ view}){
+         return view.render('index')
+    }
+
+      async logout ({ auth, response }) {
+        await auth.logout()
+        return response.route('/')
     }
 }
 
--- start/routes.js
+++ start/routes.js
@@ -18,3 +18,11 @@ const Route = use('Route')
 
 Route.on('/').render('welcome')
 Route.post('/login-store','AuthController.postLogin').as('login.store')
+Route.get('/register','AuthController.registrationView').as('register.create')
+Route.post('/register-store','AuthController.postRegister').as('register.store')
+
+Route.group(() => {
+    Route.get('/dashboard','AuthController.renderDashboard').as('dashboard')
+     Route.post('/logout','AuthController.logout').as('logout')
+   
+}).middleware(['auth'])

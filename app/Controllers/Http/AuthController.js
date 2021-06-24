3f2fa9f5e4bf4b9f692efafaeaf64203efa96c38
Mbeche <71378504+Mbeche@users.noreply.github.com>
2021-06-24 12:33:28 +0300

    creating postlogin method and routes

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -1,6 +1,13 @@
 'use strict'
 
+const User = use('App/Models/User')
+
 class AuthController {
+
+     async postLogin({ request, auth, response}) {
+        await auth.attempt(request.input('email'), request.input('password'))
+        return response.route('index')
+    }
 }
 
 module.exports = AuthController
--- start/routes.js
+++ start/routes.js
@@ -17,3 +17,4 @@
 const Route = use('Route')
 
 Route.on('/').render('welcome')
+Route.post('/login-store','AuthController.postLogin').as('login.store')
